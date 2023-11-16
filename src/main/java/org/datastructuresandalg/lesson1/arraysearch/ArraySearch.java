be350b88b95f0ad2498e14e6165e1127c2ef077a
SOUFIANE KABAD <soufiane.kabad@atos.net>
2023-11-16 11:46:41 +0100

    Lesson-1-Algorithms and Complexities

---
+++ src/main/java/org/datastructuresandalg/lesson1/arraysearch/ArraySearch.java
@@ -0,0 +1,19 @@
+package org.datastructuresandalg.lesson1.arraysearch;
+
+public class ArraySearch {
+
+    public int search(String strToMatch, String[] strArray) {
+        for (int i = 0; i < strArray.length; i++) {
+            if (strArray[i].equals(strToMatch)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArraySearch arraySearch = new ArraySearch();
+        System.out.println(arraySearch.search("Alien",
+                new String[]{"Interstellar", "District 9", "Blade Runner", "Alien", "Minority Report", "Primer"}));
+    }
+}
